3418da2a57101aba7092ba4743693b67748bbe3a
SalBouhamidi <bouhamidi.sal@gmail.com>
2023-09-14 10:21:44 +0000

    Challenge 2 : Affichage Température

--- challenge2.c
+++ challenge2.c
@@ -2,14 +2,15 @@
 int main(void){
     int F, result1, num,c;
     float n = 1.8;
-    printf("Entrer la température en Fahrenheit :  \n");
+    // E with accent is a unicode that's why i used u00E9
+    printf("Entrer la temp\u00E9rature en Fahrenheit :  \n");
     scanf("%d", &F);
     //C = (F-32)/1.8
     num = 32;
     result1 = F - num;
     c = result1 / n;
     if(c <= 0){
-        printf("très froid");
+        printf("tr\u00E8s froid");
     }
     if (c >= 1 && c < 20){
         printf("froid");
@@ -19,9 +20,8 @@ int main(void){
         printf("chaud");
     }
     if ( c > 30){
-        printf("très chaud");
+        printf("tr\u00E8s chaud");
     }
-
     return 0;
 
 }
